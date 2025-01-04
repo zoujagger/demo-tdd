4067ade69dba9e60b7f7c0c57f9b66c6d5aa3c6f
Jagger <ulrichjagger@gmail.com>
2025-01-04 20:33:00 +0100

    add feat2

--- src/main/java/com/zouj/tdd/Bibliotheque.java
+++ src/main/java/com/zouj/tdd/Bibliotheque.java
@@ -3,7 +3,9 @@ package com.zouj.tdd;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Bibliotheque {
 
@@ -15,6 +17,11 @@ public class Bibliotheque {
 
     public void ajouteBook(String... bookNames) {
         Arrays.stream(bookNames).forEach(books::add);
+    }
+
+    public List<String> arrange() {
+        // books.sort(Comparator.naturalOrder());
+        return books.stream().sorted().collect(Collectors.toList());
     }
 
 }
--- src/test/java/com/zouj/tdd/BibliothequeTest.java
+++ src/test/java/com/zouj/tdd/BibliothequeTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,8 @@ import org.junit.jupiter.api.Test;
 /**
  * Feat1: As a user, I want to add multiple books to my bookshelf so that I can
  * read them later.
+ * Feat2: As a user, I should be able to arrange my bookshelf based on certain
+ * criteria
  */
 class BibliothequeTest {
 
@@ -22,17 +25,15 @@ class BibliothequeTest {
         biblio = new Bibliotheque();
     }
 
+    // Feat1
     @Test
     void emptyBibliothequeWhenNoBookAdded() {
-        biblio = new Bibliotheque();
         List<String> books = biblio.books();
         assertTrue(books.isEmpty(), () -> "Bibliotheque should be empty.");
     }
 
     @Test
     void BibliothequeContainsTwoBookWhenTwoBooksAdded() {
-        biblio = new Bibliotheque();
-
         biblio.ajouteBook("Effective Java", "Code Complete");
         // biblio.ajouteBook("Code Complete");
 
@@ -43,8 +44,7 @@ class BibliothequeTest {
     }
 
     @Test
-    void booksReturnedFromBookShelfIsImmutableForClient() {
-        biblio = new Bibliotheque();
+    void booksReturnedFromBibliothequeIsImmutableForClient() {
         biblio.ajouteBook("Effective Java", "Code Complete");
         List<String> books = biblio.books();
         try {
@@ -53,6 +53,28 @@ class BibliothequeTest {
         } catch (Exception e) {
             assertTrue(e instanceof UnsupportedOperationException, () -> "Should throw UnsupportedOperationException.");
         }
+    }
+
+    // Feat2
+    @Test
+    void shouldArrangeBibliothequeBasedOnBookTitle() {
+
+        Bibliotheque shelf = new Bibliotheque();
+        shelf.ajouteBook("Effective Java", "Code Complete", "The Mythical Man-Month");
+        List<String> books = shelf.arrange();
+        assertEquals(Arrays.asList("Code Complete", "Effective Java", "The Mythical Man-Month"), books,
+                () -> "Books in a bookshelf should be arranged lexicographically by book title");
+
+    }
+
+    @Test
+    void booksInBibliothequeAreInInsertionOrderAfterCallingArrange() {
+        Bibliotheque shelf = new Bibliotheque();
+        shelf.ajouteBook("Effective Java", "Code Complete", "The Mythical Man-Month");
+        shelf.arrange();
+        List<String> books = shelf.books();
+        assertEquals(Arrays.asList("Effective Java", "Code Complete", "The Mythical Man-Month"), books,
+                () -> "Books in bookshelf are in insertion order");
     }
 
 }
